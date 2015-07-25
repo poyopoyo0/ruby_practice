30d5432ce220c67732387d0764e28aa0e9b6694c
poyopoyo0 <hirota.takashi@gmail.com>
2015-07-25 23:02:44 +0900

    Add 06_01.rb

---
+++ 06_01.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+99.downto(1) { |i|
+  if i == 1
+    puts i.to_s + " Bottle of beer on the wall"
+    puts i.to_s + " Bottle of beer"
+  else
+    puts i.to_s + " Bottles of beer on the wall"
+    puts i.to_s + " Bottles of beer"
+  end
+
+  puts "Take one down and pass it around"
+  puts (i-1).to_s + " Bottles of beer on the wall"
+  puts
+}
