3693bf4dfe71fb282ffc2d3d78a850b4b708f958
poyopoyo0 <hirota.takashi@gmail.com>
2015-07-25 18:43:51 +0900

    Add 05_02.rb

---
+++ 05_02.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+lineWidth = 40
+
+puts "目次".center lineWidth
+puts "1章:  数".ljust(lineWidth/2) + "p. 1".rjust(lineWidth/2)
+puts "2章:  文字".ljust(lineWidth/2) + "p. 72".rjust(lineWidth/2)
+puts "3章:  変数".ljust(lineWidth/2) + "p. 118".rjust(lineWidth/2)
