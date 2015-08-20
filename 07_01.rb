357f965c1ff2ae78ce979bd3a26e2b654722f077
poyopoyo0 <hirota.takashi@gmail.com>
2015-08-21 04:08:44 +0900

    Refactored 07_01.rb

--- 07_01.rb
+++ 07_01.rb
@@ -1,25 +1,30 @@
 # coding: utf-8
 
+def store_words
+  input = gets.chomp
+  word_array = [input]
+
+  while input != ""
+    input = gets.chomp
+    word_array << input
+
+    if word_array[-1] == ""
+      word_array.pop
+      break
+    end
+  end
+
+  return word_array
+end
+
 puts "単語を入力して下さい"
-input_word = gets.chomp
-word = [input_word]
+word = store_words
 
-while input_word != ""
-  input_word = gets.chomp
-
-  if input_word == ""
-    break
-  end
-  
-  word << input_word
+while word.size == 1 && word[0] == ""
+  puts "入力が見つかりません。入力をやり直して下さい"
+  word = store_words
 end
 
-if input_word == ""
-  if word[0] == ""
-    puts "入力がありません"
-  else
-    puts "ソートした出力を表示します"
-    puts word.sort
-    puts
-  end
-end
+puts "ソートした出力を表示します"
+puts word.sort
+puts
